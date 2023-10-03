0cb83534fe1b8a916567e8bf6d9f649bb8467fe5
DevExpressExampleBot <fakeEmail>
2023-10-03 13:35:20 +0400

    Source auto update [skip ci]

--- VB/CustomCommand/CustomCommands.vb
+++ VB/CustomCommand/CustomCommands.vb
@@ -1,7 +1,3 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Text
 Imports DevExpress.XtraRichEdit.Commands
 Imports System.Windows.Forms
 Imports DevExpress.XtraRichEdit.Services
@@ -9,49 +5,47 @@ Imports DevExpress.XtraRichEdit
 Imports DevExpress.Utils
 
 Namespace CustomCommand
-	#Region "customsavecommand"
-	Public Class CustomSaveDocumentCommand
-		Inherits SaveDocumentCommand
-		Public Sub New(ByVal control As IRichEditControl)
-			MyBase.New(control)
-		End Sub
 
-		Protected Overrides Sub ExecuteCore()
-			If Control.Document.Paragraphs.Count > 7 Then
-				MyBase.ExecuteCore()
-			Else
-				MessageBox.Show("You should type at least 7 paragraphs" & ControlChars.CrLf & "  to be able to save the document.", "Please be creative", MessageBoxButtons.OK, MessageBoxIcon.Information)
-			End If
-		End Sub
-	End Class
-	#End Region ' customsavecommand
+#Region "customsavecommand"
+    Public Class CustomSaveDocumentCommand
+        Inherits SaveDocumentCommand
 
+        Public Sub New(ByVal control As IRichEditControl)
+            MyBase.New(control)
+        End Sub
 
-	#Region "#iricheditcommandfactoryservice"
-	Public Class CustomRichEditCommandFactoryService
-		Implements IRichEditCommandFactoryService
-		Private ReadOnly service As IRichEditCommandFactoryService
-		Private ReadOnly control As RichEditControl
-
-		Public Sub New(ByVal control As RichEditControl, ByVal service As IRichEditCommandFactoryService)
-			Guard.ArgumentNotNull(control, "control")
-			Guard.ArgumentNotNull(service, "service")
-			Me.control = control
-			Me.service = service
-		End Sub
-
-		#Region "IRichEditCommandFactoryService Members"
-        Public Function CreateCommand(ByVal id As RichEditCommandId) As RichEditCommand _
-        Implements IRichEditCommandFactoryService.CreateCommand
-            If id = RichEditCommandId.FileSave Then
-                Return New CustomSaveDocumentCommand(control)
+        Protected Overrides Sub ExecuteCore()
+            If Control.Document.Paragraphs.Count > 7 Then
+                MyBase.ExecuteCore()
+            Else
+                MessageBox.Show("You should type at least 7 paragraphs
+  to be able to save the document.", "Please be creative", MessageBoxButtons.OK, MessageBoxIcon.Information)
             End If
+        End Sub
+    End Class
 
+#End Region  ' customsavecommand
+#Region "#iricheditcommandfactoryservice"
+    Public Class CustomRichEditCommandFactoryService
+        Implements IRichEditCommandFactoryService
+
+        Private ReadOnly service As IRichEditCommandFactoryService
+
+        Private ReadOnly control As RichEditControl
+
+        Public Sub New(ByVal control As RichEditControl, ByVal service As IRichEditCommandFactoryService)
+            Guard.ArgumentNotNull(control, "control")
+            Guard.ArgumentNotNull(service, "service")
+            Me.control = control
+            Me.service = service
+        End Sub
+
+#Region "IRichEditCommandFactoryService Members"
+        Public Function CreateCommand(ByVal id As RichEditCommandId) As RichEditCommand Implements IRichEditCommandFactoryService.CreateCommand
+            If id = RichEditCommandId.FileSave Then Return New CustomSaveDocumentCommand(control)
             Return service.CreateCommand(id)
         End Function
 #End Region
     End Class
-	#End Region ' #iricheditcommandfactoryservice
-
-
+#End Region  ' #iricheditcommandfactoryservice
 End Namespace
